7f8cf45b198d5a6ea6708274a2b73082fdaa84b5
fanuel <fanuelarikristians@gmail.com>
2022-10-13 23:43:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const API_KEY = 'api_key=dc30bc3cd46c397a6f8ba563d9c5120c';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+ API_KEY ;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+const SEARCH_URL = BASE_URL +'/search/movie?'+API_KEY+'&query=';
+
+const main = document.getElementById('menu');
+const form = document.getElementById('search');
+const search = document.getElementById('schBar');
+
+movieApp(API_URL);
+
+function movieApp(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results);
+        daftarFilm(data.results);
+    })
+}
+
+//Menampilkan daftar film
+function daftarFilm(data) {
+    main.innerHTML = '';
+    data.forEach(film => {
+        const {title, release_date, vote_average, poster_path, overview} = film;
+        const listFilm = document.createElement('div');
+        listFilm.classList.add('film');
+        listFilm.innerHTML = `
+            <img src="${IMG_URL+poster_path}" alt="${title}">
+            
+            <div class="infoFilm">
+                <h2>${title}</h2><span class="rating">${vote_average}</span>
+            </div>
+            <p class="release">${release_date}</p>     
+            <div class="synopsis">
+                <h2>Synopsis</h2>
+                <p>${overview}</p>                          
+            </div>
+            `
+    
+        main.appendChild(listFilm);        
+    })
+}
+
+//Menampilkan film sesuai dengan Pencarian
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let searchValue = search.value;
+
+    if(searchValue) {
+        movieApp(SEARCH_URL+searchValue);
+    }else {
+        movieApp(API_URL);
+    }
+})
+
+// // const form = document.getElementsByClassName('search')
+// // const search = document.getElementsByClassName('schBar')
+// // const movie = document.getElementsByClassName('menu')
+
+// // // Mendapatkan daftar film
+// // movieApp(API_URL) 
+// // async function movieApp() {
+// //     const app = await fetch(url)
+// //     const data = await app.json()
+// //     console.log(data.results);
+// // }
+
+// // // Menampilkan daftar film
+// // form.addEventListener('submit', (e)=>{
+// //     e.preventDefault();
+// //     const schValue = search.value;
+// //     if(schValue && schValue !==''){
+// //         movieApp(SEARCH_URL+schValue)
+// //         schValue=''
+// //     }else {
+// //         window.location.reload()
+// //     }
+// // })
+
